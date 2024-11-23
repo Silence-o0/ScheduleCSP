0fc428473c8b35e1511964df58e4cf32fb3bacaf
Olga Vishnievska <olgacherry03@gmail.com>
2024-11-23 19:31:06 +0200

    Added auditorium capacity

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import Counter, defaultdict
 from collections.abc import Callable
 from dataclasses import dataclass
 
@@ -22,7 +23,7 @@ class MustToLearn:
 
 @dataclass
 class Lesson:
-    group: str
+    group: Group
     subject: str
     teacher: str | None = None
     day: str | None = None
@@ -55,6 +56,9 @@ class CSP:
         if len(assignment) == len(csp.variables):
             return assignment, i
 
+        if i % 100 == 0:
+            print(f'{i=}')
+
         var = self.select_unassigned_variable(assignment)
 
         for value in self.domains[var]:
@@ -71,7 +75,7 @@ class CSP:
     def is_consistent(self, var, value, assignment):
         assignment[var] = value
         constraints = [teacher_time_conflict_constraint, group_time_conflict_constraint,
-                       auditorium_time_conflict_constraint]
+                       auditorium_time_conflict_constraint, auditorium_capacity_conflict_constraint]
 
         for constraint in constraints:
             if not constraint(assignment):
@@ -87,41 +91,45 @@ class CSP:
 
 
 def teacher_time_conflict_constraint(assignment: dict[MustToLearn, Lesson]):
+    same_tdl = defaultdict(list)
     for lesson in assignment.values():
-        if any(lesson.teacher == other_lesson.teacher
-               and lesson.day == other_lesson.day
-               and lesson.lesson_num == other_lesson.lesson_num
-               and (lesson.subject != other_lesson.subject
-                    or lesson.group == other_lesson.group
-                    or lesson.auditorium != other_lesson.auditorium)
-               for other_lesson in assignment.values()
+        same_tdl[lesson.teacher, lesson.day, lesson.lesson_num].append(lesson)
+
+    for lesson in assignment.values():
+        if any(lesson.subject != other_lesson.subject
+               or lesson.group == other_lesson.group
+               or lesson.auditorium != other_lesson.auditorium
+               for other_lesson in same_tdl[lesson.teacher, lesson.day, lesson.lesson_num]
                if other_lesson != lesson):
             return False
     return True
 
 
 def group_time_conflict_constraint(assignment: dict[MustToLearn, Lesson]):
-    for lesson in assignment.values():
-        if any(lesson.group == other_lesson.group
-               and lesson.day == other_lesson.day
-               and lesson.lesson_num == other_lesson.lesson_num
-               for other_lesson in assignment.values()
-               if other_lesson != lesson):
-            return False
-    return True
+    same_gdl = Counter((lesson.group, lesson.day, lesson.lesson_num) for lesson in assignment.values())
+    return same_gdl.most_common(1)[0][1] == 1
 
 
 def auditorium_time_conflict_constraint(assignment: dict[MustToLearn, Lesson]):
+    same_dla = defaultdict(list)
     for lesson in assignment.values():
-        if any(lesson.day == other_lesson.day
-               and lesson.lesson_num == other_lesson.lesson_num
-               and lesson.auditorium == other_lesson.auditorium
-               and (lesson.subject != other_lesson.subject
-                    or lesson.teacher != other_lesson.teacher)
-               for other_lesson in assignment.values()
+        same_dla[lesson.day, lesson.lesson_num, lesson.auditorium].append(lesson)
+
+    for lesson in assignment.values():
+        if any(lesson.subject != other_lesson.subject
+               or lesson.teacher != other_lesson.teacher
+               for other_lesson in same_dla[lesson.day, lesson.lesson_num, lesson.auditorium]
                if other_lesson != lesson):
             return False
     return True
+
+
+def auditorium_capacity_conflict_constraint(assignment: dict[MustToLearn, Lesson]):
+    students_in_auditoriums = Counter()
+    for lesson in assignment.values():
+        students_in_auditoriums[lesson.auditorium] += lesson.group.students
+
+    return all(students <= auditorium.capacity for auditorium, students in students_in_auditoriums.items())
 
 
 if __name__ == "__main__":
@@ -135,10 +143,6 @@ if __name__ == "__main__":
         Group("К-15", 18): ["АГ", "Мат аналіз", "Програмування", "Дискретна математика", "Англійська мова", "ВДУС"],
         Group("К-16", 28): ["АГ", "Мат аналіз", "Програмування", "Дискретна математика", "Англійська мова", "ВДУС"],
         Group("К-17", 25): ["АГ", "Мат аналіз", "Програмування", "Дискретна математика", "Англійська мова", "ВДУС"],
-        Group("Група1", 36): ["Дисципліна1", "Дисципліна2", "Дисципліна3", "Дисципліна4", "Англійська мова", "ВДУС"],
-        Group("Група2", 32): ["Дисципліна1", "Дисципліна3", "Дисципліна5", "Дисципліна4"],
-        Group("Група3", 24): ["Дисципліна2", "Дисципліна3", "Дисципліна4", "Англійська мова"],
-        Group("Група4", 29): ["Дисципліна2", "Дисципліна4"],
     }
 
     subject_teachers_dict = {
@@ -154,19 +158,14 @@ if __name__ == "__main__":
         "Дискретна математика": ["Коваль", "Веклич"],
         "Англійська мова": ["Паламарчук"],
         "ВДУС": ["Набока"],
-        "Дисципліна1": ["Викладач1", "Викладач2"],
-        "Дисципліна2": ["Викладач2", "Викладач3"],
-        "Дисципліна3": ["Викладач4"],
-        "Дисципліна4": ["Викладач2", "Викладач4"],
-        "Дисципліна5": ["Викладач4"],
     }
 
     # day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
     day = ["Monday", "Tuesday"]
-    lesson_num = [1, 2, 3, 4]
+    lesson_num = [1, 2, 3]
     auditorium = [Auditorium("39", 300), Auditorium("43", 300),
-                  Auditorium("302", 70), Auditorium("303", 70),
-                  Auditorium("201", 50), Auditorium("202", 50), Auditorium("203", 50), Auditorium("204", 50),
+                  Auditorium("302", 60), Auditorium("303", 60),
+                  Auditorium("201", 50), Auditorium("202", 50), Auditorium("203", 50),
                   ]
 
     X = []
@@ -188,6 +187,7 @@ if __name__ == "__main__":
             for d in day
             for l in lesson_num
             for a in auditorium
+            if a.capacity >= group.students
         ]
 
         D[x_comb] = possible_values
